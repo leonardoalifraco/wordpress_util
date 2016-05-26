739744e5559ed05383104df11495772ded1c45a1
Leonardo Alifraco <leonardo.alifraco@gmail.com>
2016-05-25 22:30:22 -0300

    Fix bug with script and style tags.

--- spec/wordpress_util_spec.rb
+++ spec/wordpress_util_spec.rb
@@ -110,5 +110,50 @@ line 5</p>"
 
       expect(WordpressUtil.wpautop(content).strip).to eq expected
     end
+
+    it "should not add line breaks between script tags" do
+      content = "<script type=\"text/javascript\">\n"
+      content += "alert(\"This is a test alert\");\n"
+      content += "</script>"
+
+      expected = "<p>" + content + "</p>"
+
+      expect(WordpressUtil.wpautop(content).strip).to eq expected
+    end
+
+    it "should process ok with script tags on it" do
+      content = "[header]72 Beverly Park - BEVERLY HILLS, CA, 90210[/header]\n"
+      content += "<div id=\"cp_widget_2ae738fb-0bd7-4dde-a359-2b3beccfc25e\">...</div><script type=\"text/javascript\">\n"
+      content += "var cpo = [];\n"
+      content += "cpo[\"_object\"] =\"cp_widget_2ae738fb-0bd7-4dde-a359-2b3beccfc25e\"; cpo[\"_fid\"] = \"AwCAWcNQsG3w\";\n"
+      content += "var _cpmp = _cpmp || []; _cpmp.push(cpo);\n"
+      content += "(function() { var cp = document.createElement(\"script\"); cp.type = \"text/javascript\";\n"
+      content += "cp.async = true; cp.src = \"//www.cincopa.com/media-platform/runtime/libasync.js\";\n"
+      content += "var c = document.getElementsByTagName(\"script\")[0];\n"
+      content += "c.parentNode.insertBefore(cp, c); })();\n"
+      content += "</script>\n"
+      content += "<noscript>Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video Hosting Platform</a> for Business solution.<span>Beverly Hills - $250 Mill Alt</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 869</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 1087</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 1073</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 1132</span></noscript>\n"
+      content += "<strong>Price:</strong> $45 Million\n"
+      content += "<strong>Bedrooms:</strong> 11\n"
+      content += "<strong>Bathrooms:</strong> 10 Full, 6 Partial"
+
+      expected = "<p>[header]72 Beverly Park - BEVERLY HILLS, CA, 90210[/header]</p>\n"
+      expected += "<div id=\"cp_widget_2ae738fb-0bd7-4dde-a359-2b3beccfc25e\">...</div>\n"
+      expected += "<p><script type=\"text/javascript\">\n"
+      expected += "var cpo = [];\n"
+      expected += "cpo[\"_object\"] =\"cp_widget_2ae738fb-0bd7-4dde-a359-2b3beccfc25e\"; cpo[\"_fid\"] = \"AwCAWcNQsG3w\";\n"
+      expected += "var _cpmp = _cpmp || []; _cpmp.push(cpo);\n"
+      expected += "(function() { var cp = document.createElement(\"script\"); cp.type = \"text/javascript\";\n"
+      expected += "cp.async = true; cp.src = \"//www.cincopa.com/media-platform/runtime/libasync.js\";\n"
+      expected += "var c = document.getElementsByTagName(\"script\")[0];\n"
+      expected += "c.parentNode.insertBefore(cp, c); })();\n"
+      expected += "</script><br />\n"
+      expected += "<noscript>Powered by Cincopa <a href='http://www.cincopa.com/video-hosting'>Video Hosting Platform</a> for Business solution.<span>Beverly Hills - $250 Mill Alt</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 869</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 1087</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 1073</span><span>originaldate</span><span> 1/1/0001 6:00:00 AM</span><span>width</span><span> 1700</span><span>height</span><span> 1132</span></noscript><br />\n"
+      expected += "<strong>Price:</strong> $45 Million<br />\n"
+      expected += "<strong>Bedrooms:</strong> 11<br />\n"
+      expected += "<strong>Bathrooms:</strong> 10 Full, 6 Partial</p>"
+
+      expect(WordpressUtil.wpautop(content).strip).to eq expected
+    end
   end
 end
